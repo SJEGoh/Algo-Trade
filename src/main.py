03cb2b072509a35c9e628d4b066e07843810377e
SJEGoh <57899746+SJEGoh@users.noreply.github.com>
2025-12-07 22:34:26 +0800

    backup

---
+++ src/main.py
@@ -0,0 +1,85 @@
+from pair import PairModel, PairTrader, Portfolio
+from HypoTest import RegimeDetector
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import numpy as np
+
+
+def backtest(df1, df2, h, K):
+    prices = pd.concat([df1,df2], axis = 1).dropna()
+    prices.columns = ["S1", "S2"]
+    portfolio = Portfolio(100)
+
+    train_i = 10
+    train_set = prices.iloc[:train_i]
+    test_set = prices.iloc[train_i:]
+    train_rows = []   # collect rows here
+
+    pair_model = PairModel()
+    pair_model.fit_hedge(train_set)
+
+    for i, row in train_set.iterrows():
+        spread = pair_model.compute_spread(row["S1"], row["S2"])
+        train_rows.append((i, spread))
+
+    train_spread = pd.DataFrame(train_rows, columns=["Date", "Spread"]).set_index("Date")
+    
+    detector = RegimeDetector(h = h, K = K)
+    detector.initialize(train_spread["Spread"])
+    strategy = PairTrader(2, 0.5, train_spread["Spread"].mean(), train_spread["Spread"].std())
+    last_k = train_set.tail(5)
+    r = 0
+    for i, row in test_set.iterrows():
+        spread = pair_model.compute_spread(row["S1"], row["S2"])
+        last_k = last_k.iloc[1:]
+        last_k.loc[i] = row
+
+        last_k_spread = []
+        if rg := detector.update(spread):
+            r += 1
+            pair_model.fit_hedge(last_k)
+            for j, row_k in last_k.iterrows():
+                spread = pair_model.compute_spread(row_k["S1"], row_k["S2"])
+                last_k_spread.append((j, spread))
+            last_k_spread = pd.DataFrame(last_k_spread, columns=["Date", "Spread"]).set_index("Date")
+            detector.reset_baseline(last_k_spread["Spread"])
+            strategy.set_params(detector.get_params())
+        position = strategy.generate(spread, rg)
+        prices = {"S1": row["S1"], "S2": row["S2"]}
+        if detector.L < 0:
+            print("L =", detector.L)
+        portfolio.update_position(i, prices, position, pair_model.b)
+
+    d = pd.DataFrame(portfolio.history)
+    d.columns = ["Date", "Value"]
+    d = d.set_index("Date")
+    daily_return = d.copy().pct_change()
+    mean = daily_return.mean()
+    sd = daily_return.std()
+    cumu_return = d.iloc[-1]/d.iloc[0] - 1
+    sd = sd * np.sqrt(252)
+    rfr = 0.02
+    sharpe = (cumu_return-rfr)/sd
+    print(sharpe)
+    d.plot()
+    plt.show()
+
+
+
+
+tickers = ["KO", "PEP"]
+
+S1_ticker = yf.Ticker(tickers[0])
+S2_ticker = yf.Ticker(tickers[1])
+
+S1_data = S1_ticker.history(period = '15y')[["Close"]]
+S2_data = S2_ticker.history(period = '15y')[["Close"]]
+
+
+print(backtest(S1_data, S2_data, h = 0.001, K = 2))
+
+# MSFT + ADBE, 0.004, 5, 10 years, 10 lmao
+# KO + PEP, 0.001, 2, 15 years, 2.454, z-entry = 2, z-exit = 0.75
+# BTC, ETH, 0.05, 10, 1 year, 2.05, z-entry = 2, z-exit = 0.75
---
+++ src/naive_pairs.py
@@ -0,0 +1,116 @@
+
+'''
+Assumptions for naive pairs trading model:
+- Both stocks are cointegrated 
+- Variance and Mean constant
+'''
+
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+tickers = ["BTC-USD", "ETH-USD"]
+
+S1_ticker = yf.Ticker(tickers[0])
+S2_ticker = yf.Ticker(tickers[1])
+
+S1_data = S1_ticker.history(period = '1y')[["Close"]]
+S2_data = S2_ticker.history(period = '1y')[["Close"]]
+'''
+S1_data = sm.add_constant(S1_data)
+results = sm.OLS(S2_data, S1_data).fit()
+S1_data = S1_data.drop("const", axis = 1)
+b = results.params[0]
+
+
+norm = zscore(spread)
+norm.plot()
+plt.axhline(1, color='black')
+plt.axhline(0, color='black')
+plt.axhline(-1, color='black')
+plt.show()
+
+
+Idea is to buy when larger than 1 and sell when lower than 1.
+This example has a lot of problems such as look ahead bias and assuming that
+this relationship will hold for any different time period. In fact, changing the
+time period to anything else will show a funny looking graph. 
+'''
+
+def zscore(series, mu, sd):
+    return (series-mu)/sd
+
+def backtest(S1, S2, test_prop):
+    if not test_prop:
+        return 0
+    S1 = S1.reset_index().merge(S2.reset_index(), on = "Date", how = "inner", suffixes = ["_S1", "_S2"]).set_index("Date")
+    split = int(len(S1) * test_prop)
+    train = S1.iloc[:split]
+    test  = S1.iloc[split:]
+    x = sm.add_constant(train["Close_S1"])
+    results = sm.OLS(train["Close_S2"], x).fit()
+    b = results.params["Close_S1"]
+
+    train_spread = train["Close_S2"] - b * train["Close_S1"] - results.params["const"]
+    mu = train_spread.mean()
+    sd = train_spread.std()
+    test_spread = test["Close_S2"] - b * test["Close_S1"] - results.params["const"]
+    
+    test_zscores = zscore(test_spread, mu, sd)
+
+    money = 100
+    S1_holding = 0
+    S2_holding = 0
+
+    records = []
+    for i, z in enumerate(test_zscores):
+        if z < -1 and (not S2_holding and not S1_holding):
+            tot = test["Close_S1"].iloc[i] * b + test["Close_S2"].iloc[i]
+            S1_holding -= b * 100/tot
+            S2_holding += 100/tot
+            money += b * 100/tot * test["Close_S1"].iloc[i] - 100/tot * test["Close_S2"].iloc[i]
+            continue
+        if z > 1 and (not S2_holding and not S1_holding):
+            tot = test["Close_S1"].iloc[i] * b + test["Close_S2"].iloc[i]
+            S1_holding += b * 100/tot
+            S2_holding -= 100/tot
+            money -= b * 100/tot * test["Close_S1"].iloc[i] - 100/tot * test["Close_S2"].iloc[i]
+            continue
+        if abs(z) < 0.5:
+            money += S1_holding * test["Close_S1"].iloc[i] + S2_holding * test["Close_S2"].iloc[i]
+            S1_holding = 0
+            S2_holding = 0
+        records.append({
+            "Date": test.index[i],
+            "S1": S1_holding,
+            "S2": S2_holding,
+            "Money": money
+        })
+    if S1_holding != 0 or S2_holding != 0:
+        p1 = test["Close_S1"].iloc[-1]
+        p2 = test["Close_S2"].iloc[-1]
+        money += S1_holding * p1 + S2_holding * p2
+    records.append({
+        "Date": test.index[-1],
+        "S1": S1_holding,
+        "S2": S2_holding,
+        "Money": money
+    })
+    
+    return pd.DataFrame(records).set_index("Date")
+
+
+print(data := backtest(S1_data, S2_data, 0.2))
+
+
+data[["Money"]].plot()
+plt.show()
+
+
+
+
+
+    
+
+
